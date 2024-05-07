bf1bdda6e2081364dd396f1e637f4de5bfe03930
HarshGaurup <harshitgaur444@gmail.com>
2024-05-07 20:40:21 +0530

    first commit

---
+++ client/src/utils/styles.js
@@ -0,0 +1,66 @@
+export const isActiveStyles =
+  " text-2xl text-red-700 font-semibold hover:text-red-700 px-4 py-2 duration-100 transition-all ease-in-out";
+
+export const isNotActiveStyles =
+  " text-xl text-textColor hover:text-red-700 duration-100 px-4 py-2 transition-all ease-in-out";
+
+export const statuses = [
+  { id: 1, title: "Drinks", category: "drinks" },
+  { id: 2, title: "Deserts", category: "deserts" },
+  { id: 3, title: "Fruits", category: "fruits" },
+  { id: 4, title: "Rice", category: "rice" },
+  { id: 5, title: "Curry", category: "curry" },
+  { id: 6, title: "Chinese", category: "chinese" },
+  { id: 7, title: "Bread", category: "bread" },
+];
+
+export const randomData = [
+  {
+    id: 1,
+    imageURL:
+      "https://firebasestorage.googleapis.com/v0/b/foodapp-117b9.appspot.com/o/Images%2F1715076564436_i6.png?alt=media&token=e24fc984-27f7-4a4c-b3b9-b8ad106411ab",
+    product_name: "Iceream",
+    product_category: "Chocolate & vanilla",
+    product_price: "23.00",
+  },
+  {
+    id: 2,
+    imageURL:
+      "https://firebasestorage.googleapis.com/v0/b/foodapp-117b9.appspot.com/o/Images%2F1715076224377_f1.png?alt=media&token=b7493c18-c8d6-431e-820c-f68141d8ee9f",
+    product_name: "Strawberries",
+    product_category: "Fresh Strawberries",
+    product_price: "18.00",
+  },
+  {
+    id: 3,
+    imageURL:
+      "https://firebasestorage.googleapis.com/v0/b/foodapp-117b9.appspot.com/o/Images%2F1715076256785_f2.png?alt=media&token=cbfd6865-0803-491b-a357-071f3a8b8a68",
+    product_name: "Pine Apple",
+    product_category: "Juicy Apples",
+    product_price: "16.00",
+  },
+  {
+    id: 4,
+    imageURL:
+    "https://firebasestorage.googleapis.com/v0/b/foodapp-117b9.appspot.com/o/Images%2F1715075683654_c3.png?alt=media&token=7650d2f6-2a3e-46ad-af7e-25fac1671aee",
+    product_name: "Chicken Kebab",
+    product_category: "Mixed Kebab Plate",
+    product_price: "22.0",
+  },
+  {
+    id: 5,
+    imageURL:
+      "https://firebasestorage.googleapis.com/v0/b/foodapp-117b9.appspot.com/o/Images%2F1715077357722_d4.png?alt=media&token=af055906-b266-4cc9-93c8-06c9472fe1b4",
+    product_name: "Mojito",
+    product_category: "Cocktail Drinks",
+    product_price: "10.0",
+  },
+  {
+    id: 6,
+    imageURL:
+      "https://firebasestorage.googleapis.com/v0/b/foodapp-117b9.appspot.com/o/Images%2F1715077448603_fi3.png?alt=media&token=a98d0175-6f68-4e0e-b05c-a23014c0e19a",
+    product_name: "Prawn Masala",
+    product_category: "Spicy Masala",
+    product_price: "20.0",
+  },
+];
